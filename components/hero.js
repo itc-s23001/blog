be8a9603c1947660ab6043f8683583ca8c40207f
itc-s23001 <s23001@std.it-college.ac.jp>
2023-12-14 09:15:09 +0000

    chapter2
    chapter2を終わらせた

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,9 @@
-export default function Hero () {
-  return (
-    <div>
-      <h1>CUBE</h1>
-      <p>アウトプットしていくサイト</p>
-    </div>
-  )
-}
+const Hero = ({ title, subtitle, imageOn = false }) => (
+  <div>
+    <h1>{title}</h1>
+    <p>{subtitle}</p>
+    {imageOn && <figure> [image] </figure>}
+  </div>
+)
+
+export default Hero
